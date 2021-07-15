709976f5551a6f25e8f5f5d6fb28f19f3a0bed20
Sam Xu <962226532@qq.com>
2021-07-15 11:22:33 +0800

    Update diy-part2-lean.sh

--- diy-part2-lean.sh
+++ diy-part2-lean.sh
@@ -13,7 +13,7 @@
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 
-# 修改默认IP
+# 修改默认ip
 sed -i 's/192.168.1.1/192.168.1.253/g' package/base-files/files/bin/config_generate
 # 修改默认主机名称
 sed -i 's/OpenWrt/SamWrt/g' package/base-files/files/bin/config_generate
@@ -29,15 +29,19 @@ sed -i 's/\(option sequential_ip\)/\t\1/' package/network/services/dnsmasq/files
 sed -i '/start/s/100/20/g' package/network/services/dnsmasq/files/dhcp.conf
 sed -i '/limit/s/150/50/g' package/network/services/dnsmasq/files/dhcp.conf
 # 支持iptv
-sed -i '$a dhcp-option-force=125,00:00:00:00:1a:02:06:48:47:57:2d:43:54:03:04:5a:58:48:4e:0a:02:20:00:0b:02:00:55:0d:02:00:2e' package/network/services/dnsmasq/files/dnsmasq.conf
-sed -i '$a dhcp-option=15' package/network/services/dnsmasq/files/dnsmasq.conf
-sed -i '$a dhcp-option=28' package/network/services/dnsmasq/files/dnsmasq.conf
-sed -i '$a dhcp-option=60,00:00:01:06:68:75:61:71:69:6E:02:0A:48:47:55:34:32:31:4E:20:76:33:03:0A:48:47:55:34:32:31:4E:20:76:33:04:10:32:30:30:2E:55:59:59:2E:30:2E:41:2E:30:2E:53:48:05:04:00:01:00:50' package/network/services/dnsmasq/files/dnsmasq.conf
+#sed -i '$a dhcp-option-force=125,00:00:00:00:1a:02:06:48:47:57:2d:43:54:03:04:5a:58:48:4e:0a:02:20:00:0b:02:00:55:0d:02:00:2e' package/network/services/dnsmasq/files/dnsmasq.conf
+#sed -i '$a dhcp-option=15' package/network/services/dnsmasq/files/dnsmasq.conf
+#sed -i '$a dhcp-option=28' package/network/services/dnsmasq/files/dnsmasq.conf
+#sed -i '$a dhcp-option=60,00:00:01:06:68:75:61:71:69:6E:02:0A:48:47:55:34:32:31:4E:20:76:33:03:0A:48:47:55:34:32:31:4E:20:76:33:04:10:32:30:30:2E:55:59:59:2E:30:2E:41:2E:30:2E:53:48:05:04:00:01:00:50' package/network/services/dnsmasq/files/dnsmasq.conf
+echo 'dhcp-option-force=125,00:00:00:00:1a:02:06:48:47:57:2d:43:54:03:04:5a:58:48:4e:0a:02:20:00:0b:02:00:55:0d:02:00:2e' >>package/network/services/dnsmasq/files/dnsmasq.conf
+echo 'dhcp-option=15' >>package/network/services/dnsmasq/files/dnsmasq.conf
+echo 'dhcp-option=28' >>package/network/services/dnsmasq/files/dnsmasq.conf
+echo 'dhcp-option=60,00:00:01:06:68:75:61:71:69:6E:02:0A:48:47:55:34:32:31:4E:20:76:33:03:0A:48:47:55:34:32:31:4E:20:76:33:04:10:32:30:30:2E:55:59:59:2E:30:2E:41:2E:30:2E:53:48:05:04:00:01:00:50' >>package/network/services/dnsmasq/files/dnsmasq.conf
+# 修改默认root密码
+sed -i 's#root::0#root:$1$yW9piKyc$OT6rrlpcoPRvf1Vk.Zm9N/:18415#g' package/base-files/files/etc/shadow
 # turboacc优化
 sed -i '/bbr/s/0/1/g' package/lean/luci-app-turboacc/root/etc/config/turboacc
 sed -i '/bridge/s/1/0/g' package/lean/luci-app-turboacc/root/etc/config/turboacc
-# 修改默认root密码
-sed -i 's#root::0#root:$1$yW9piKyc$OT6rrlpcoPRvf1Vk.Zm9N/:18415#g' package/base-files/files/etc/shadow
 # 允许外网访问
 sed -i 's/rfc1918_filter 1/rfc1918_filter 0/g' package/network/services/uhttpd/files/uhttpd.config
 # 开启upnp
