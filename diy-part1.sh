39fff8b4a8ffc263d3262517c383a19156c2f971
xzhhzx222 <xzhhzx222@gmail.com>
2022-07-17 20:52:40 +0800

    update

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,3 +16,31 @@
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+
+# 添加passwall
+echo 'src-git passwall_pkg https://github.com/xiaorouji/openwrt-passwall;packages' >>feeds.conf.default
+echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall;luci' >>feeds.conf.default
+echo 'src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2' >>feeds.conf.default
+# 添加ssrplus
+echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+
+# 添加self-settings
+svn co https://github.com/xzhhzx222/Actions-OpenWrt/trunk/diy package/self-settings
+
+# 添加advanced
+git clone https://github.com/sirpdboy/luci-app-advanced.git package/luci-app-advanced
+# 添加control
+svn co https://github.com/Lienol/openwrt-package/trunk/luci-app-control-timewol package/samwrt/luci-app-control-timewol
+svn co https://github.com/Lienol/openwrt-package/trunk/luci-app-control-weburl package/samwrt/luci-app-control-weburl
+# 添加helloworld
+git clone https://github.com/jerrykuku/lua-maxminddb.git package/samwrt/lua-maxminddb
+git clone https://github.com/jerrykuku/luci-app-vssr.git package/samwrt/luci-app-vssr
+# 添加openclash
+svn co https://github.com/vernesong/OpenClash/trunk/luci-app-openclash package/samwrt/luci-app-openclash
+# 添加timecontrol
+svn co https://github.com/Lienol/openwrt-package/trunk/luci-app-timecontrol package/samwrt/luci-app-timecontrol
+# 添加wolplus
+svn co https://github.com/sundaqiang/openwrt-packages/trunk/luci-app-wolplus package/luci-app-wolplus
+
+# 添加argon
+git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon-18.06
---
+++ diy/diy-part2-jimmywrt.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#
+# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
+#
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
+#
+# https://github.com/P3TERX/Actions-OpenWrt
+# File name: diy-part2.sh
+# Description: OpenWrt DIY script part 2 (After Update feeds)
+#
+
+# Modify default IP
+#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+
+# 更改默认主题
+sed -i 's/luci-theme-bootstrap/luci-theme-argon-18.06/g' package/feeds/luci/luci/Makefile
+
+# 替换default-settings
+mv -vf package/self-settings/default-settings/jimmywrt-settings package/lean/default-settings/files/zzz-default-settings
+
+# 替换serverchan logo
+mv -vf package/self-settings/serverchan/jimmywrt.jpg package/feeds/luci/luci-app-serverchan/root/usr/bin/serverchan/api/logo.jpg
+
+# 删除多余配置
+rm -rf package/self-settings
