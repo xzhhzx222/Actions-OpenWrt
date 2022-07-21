f6c5ffd32797b5e18def3b563679b3566b3737fb
Sam Xu <xzhhzx222@gmail.com>
2022-07-21 15:12:46 +0800

    Update diy-part2-samwrt.sh

--- diy/diy-part2-samwrt.sh
+++ diy/diy-part2-samwrt.sh
@@ -14,7 +14,8 @@
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 
 # 更改默认主题
-sed -i 's/luci-theme-bootstrap/luci-theme-argon-18.06/g' package/feeds/luci/luci/Makefile
+# sed -i 's/luci-theme-bootstrap/luci-theme-argon-18.06/g' package/feeds/luci/luci/Makefile
+sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' package/feeds/luci/luci/Makefile
 
 # 替换default-settings
 mv -vf package/self-settings/default-settings/samwrt-settings package/lean/default-settings/files/zzz-default-settings
