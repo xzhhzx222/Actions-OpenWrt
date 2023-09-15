52a3da86dcf01b48e5d9d99e9fec6ec162df6290
Sam Xu <xzhhzx222@gmail.com>
2023-09-15 12:38:18 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -14,10 +14,11 @@
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 
 # 更改默认主题
-sed -i 's/luci-theme-bootstrap/luci-theme-argon-18.06/g' package/feeds/luci/luci/Makefile
+rm -rf package/feeds/luci/luci-theme-argon
+sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' package/feeds/luci/luci/Makefile
 
 # 删除serverchan
 rm -rf package/feeds/luci/luci-app-serverchan
 
 # 替换update_cloudflare_com_v4.sh
-#wget -O package/feeds/packages/ddns-scripts/files/update_cloudflare_com_v4.sh https://raw.githubusercontent.com/openwrt/packages/master/net/ddns-scripts/files/usr/lib/ddns/update_cloudflare_com_v4.sh
+# wget -O package/feeds/packages/ddns-scripts/files/update_cloudflare_com_v4.sh https://raw.githubusercontent.com/openwrt/packages/master/net/ddns-scripts/files/usr/lib/ddns/update_cloudflare_com_v4.sh
