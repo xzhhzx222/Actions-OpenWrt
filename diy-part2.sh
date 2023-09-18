7d6060cb77d435ff716597fcea7b7389203a8aa1
Sam Xu <xzhhzx222@gmail.com>
2023-09-18 22:25:53 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -13,15 +13,15 @@
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 
-# # 更改默认主题
+# 更改默认主题
 # rm -rf package/feeds/luci/luci-theme-argon
-# sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' package/feeds/luci/luci/Makefile
+sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' package/feeds/luci/luci/Makefile
 
 # # 更改编译语言
 # sed -i '/LUCI_DEPENDS:=/s/$/ +@LUCI_LANG_zh_Hans/' package/feeds/luci/luci-base/Makefile
 
-# # 删除serverchan
-# rm -rf package/feeds/luci/luci-app-serverchan
+# 删除serverchan
+rm -rf package/feeds/luci/luci-app-serverchan
 
 # 替换update_cloudflare_com_v4.sh
 # wget -O package/feeds/packages/ddns-scripts/files/update_cloudflare_com_v4.sh https://raw.githubusercontent.com/openwrt/packages/master/net/ddns-scripts/files/usr/lib/ddns/update_cloudflare_com_v4.sh
