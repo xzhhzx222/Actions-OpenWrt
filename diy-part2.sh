a8bc64c5b1f4dc6fcac27d054a569e50f6f066c4
Sam Xu <xzhhzx222@gmail.com>
2023-09-20 10:04:53 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -14,23 +14,23 @@
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 
 if [ "$BUILD_BRANCH" == "openwrt-*" ]; then
-	LOGO_FILE=package/xzhhzx222/luci-app-wechatpush/root/usr/share/wechatpush/api/logo.jpg
-	SET_FILE=package/default-settings/files/zzz-default-settings
-	svn export https://github.com/Lienol/openwrt/branches/22.03/package/default-settings package/default-settings
-	git clone https://github.com/tty228/luci-app-wechatpush.git package/xzhhzx222/luci-app-wechatpush
-	git clone https://github.com/jerrykuku/luci-theme-argon.git package/xzhhzx222/luci-theme-argon
-	sed -i '/LUCI_DEPENDS:=/s/$/ +@LUCI_LANG_zh_Hans/' package/feeds/luci/luci-base/Makefile
-	for pkg in $(find package/feeds/*/luci-app*/po -maxdepth 0 -type d); do
-	if [ ! -d "$pkg/zh_Hans" ] && [ -d "$pkg/zh-cn" ]; then 
-		ln -sf zh-cn "$pkg/zh_Hans"
-	fi
-done
+  LOGO_FILE=package/xzhhzx222/luci-app-wechatpush/root/usr/share/wechatpush/api/logo.jpg
+  SET_FILE=package/default-settings/files/zzz-default-settings
+  svn export https://github.com/Lienol/openwrt/branches/22.03/package/default-settings package/default-settings
+  git clone https://github.com/tty228/luci-app-wechatpush.git package/xzhhzx222/luci-app-wechatpush
+  git clone https://github.com/jerrykuku/luci-theme-argon.git package/xzhhzx222/luci-theme-argon
+  sed -i '/LUCI_DEPENDS:=/s/$/ +@LUCI_LANG_zh_Hans/' package/feeds/luci/luci-base/Makefile
+  for pkg in $(find package/feeds/*/luci-app*/po -maxdepth 0 -type d); do
+    if [ ! -d "$pkg/zh_Hans" ] && [ -d "$pkg/zh-cn" ]; then 
+      ln -sf zh-cn "$pkg/zh_Hans"
+    fi
+  done
 elif [ "$BUILD_BRANCH" == "lede" ]; then
-	LOGO_FILE=package/xzhhzx222/luci-app-wechatpush/root/usr/share/serverchan/api/logo.jpg
-	SET_FILE=package/lean/default-settings/files/zzz-default-settings
-	rm -rf package/feeds/luci/luci-theme-argon
-	git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/xzhhzx222/luci-theme-argon
-	git clone -b openwrt-18.06 https://github.com/tty228/luci-app-wechatpush.git package/xzhhzx222/luci-app-wechatpush
+  LOGO_FILE=package/xzhhzx222/luci-app-wechatpush/root/usr/share/serverchan/api/logo.jpg
+  SET_FILE=package/lean/default-settings/files/zzz-default-settings
+  rm -rf package/feeds/luci/luci-theme-argon
+  git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/xzhhzx222/luci-theme-argon
+  git clone -b openwrt-18.06 https://github.com/tty228/luci-app-wechatpush.git package/xzhhzx222/luci-app-wechatpush
 fi
 
 [ -e $DIY_LOGO ] && mv -vf $DIY_LOGO $LOGO_FILE
