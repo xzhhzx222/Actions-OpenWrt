143a844cd988434a123839ceea5a50bb65b620bd
Sam Xu <xzhhzx222@gmail.com>
2023-09-25 17:28:30 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -22,7 +22,7 @@ echo "DIY_SET=$DIY_SET"
 echo "DIY_LOGO=$DIY_LOGO"
 echo "------------- Check End -------------"
 
-if [[ $BUILD_BRANCH == openwrt-* ]]; then
+if [ $BUILD_BRANCH = openwrt-* ]; then
   LOGO_FILE=package/xzhhzx222/luci-app-wechatpush/root/usr/share/wechatpush/api/logo.jpg
   SET_FILE=package/default-settings/files/zzz-default-settings
   svn export https://github.com/Lienol/openwrt/branches/22.03/package/default-settings package/default-settings
@@ -34,7 +34,7 @@ if [[ $BUILD_BRANCH == openwrt-* ]]; then
       ln -sf zh-cn "$pkg/zh_Hans"
     fi
   done
-elif [[ $BUILD_BRANCH == lede ]]; then
+elif [ $BUILD_BRANCH = lede ]; then
   LOGO_FILE=package/xzhhzx222/luci-app-wechatpush/root/usr/share/serverchan/api/logo.jpg
   SET_FILE=package/lean/default-settings/files/zzz-default-settings
   # wget -O package/feeds/packages/ddns-scripts/files/update_cloudflare_com_v4.sh https://raw.githubusercontent.com/openwrt/packages/master/net/ddns-scripts/files/usr/lib/ddns/update_cloudflare_com_v4.sh
