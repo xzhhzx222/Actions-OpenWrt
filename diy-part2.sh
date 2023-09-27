18f56b8b190a7e38a7483676b76842e94d39251a
xzhhzx222 <xzhhzx222@gmail.com>
2023-09-27 22:38:02 +0800

    update

--- diy-part2.sh
+++ diy-part2.sh
@@ -22,42 +22,58 @@ echo "DIY_SET=$DIY_SET"
 echo "DIY_LOGO=$DIY_LOGO"
 echo "------------- Check End -------------"
 
-if [[ $BUILD_BRANCH == openwrt-* ]]; then
-  LOGO_FILE=package/xzhhzx222/luci-app-wechatpush/root/usr/share/wechatpush/api/logo.jpg
-  SET_FILE=package/default-settings/files/zzz-default-settings
-  rm -rf package/network/config/firewall
-  rm -rf package/network/utils/iptables
-  rm -rf package/feeds/luci/luci-app-firewall
-  rm -rf package/feeds/lienol/luci-app-softethervpn/
-  #svn export https://github.com/coolsnowwolf/lede/trunk/package/network/config/firewall package/network/config/firewall
-  svn export https://github.com/immortalwrt/immortalwrt/trunk/package/network/config/firewall package/network/config/firewall
-  # svn export https://github.com/coolsnowwolf/lede/trunk/package/network/utils/iptables package/network/utils/iptables
-  svn export https://github.com/immortalwrt/immortalwrt/trunk/package/network/utils/iptables package/network/utils/iptables
-  # svn export https://github.com/coolsnowwolf/lede/trunk/package/network/services/fullconenat package/network/services/fullconenat
-  svn export https://github.com/immortalwrt/immortalwrt/trunk/package/network/utils/fullconenat package/network/utils/fullconenat
-  # svn export https://github.com/coolsnowwolf/luci/trunk/applications/luci-app-firewall package/feeds/luci/luci-app-firewall
-  svn export https://github.com/immortalwrt/luci/trunk/applications/luci-app-firewall package/feeds/luci/luci-app-firewall
-  # svn export https://github.com/coolsnowwolf/luci/trunk/luci.mk package/feeds/luci.mk
-  svn export https://github.com/immortalwrt/luci/trunk/luci.mk package/feeds/luci.mk
-   svn export https://github.com/Lienol/openwrt/branches/22.03/package/default-settings package/default-settings
-  #svn export https://github.com/coolsnowwolf/lede/trunk/package/lean/default-settings package/default-settings
-  svn export https://github.com/coolsnowwolf/luci/trunk/applications/luci-app-softethervpn package/xzhhzx222/luci-app-softethervpn
-  git clone https://github.com/tty228/luci-app-wechatpush.git package/xzhhzx222/luci-app-wechatpush
-  git clone https://github.com/jerrykuku/luci-theme-argon.git package/xzhhzx222/luci-theme-argon
-  sed -i '/LUCI_DEPENDS:=/s/$/ +@LUCI_LANG_zh_Hans/' package/feeds/luci/luci-base/Makefile
-  for pkg in $(find package/feeds/*/luci-app*/po -maxdepth 0 -type d); do
-    if [ ! -d "$pkg/zh_Hans" ] && [ -d "$pkg/zh-cn" ]; then 
-      ln -sf zh-cn "$pkg/zh_Hans"
-    fi
-  done
-elif [ $BUILD_BRANCH = lede ]; then
-  LOGO_FILE=package/xzhhzx222/luci-app-wechatpush/root/usr/share/serverchan/api/logo.jpg
-  SET_FILE=package/lean/default-settings/files/zzz-default-settings
-  # wget -O package/feeds/packages/ddns-scripts/files/update_cloudflare_com_v4.sh https://raw.githubusercontent.com/openwrt/packages/master/net/ddns-scripts/files/usr/lib/ddns/update_cloudflare_com_v4.sh
-  git clone -b openwrt-18.06 https://github.com/tty228/luci-app-wechatpush.git package/xzhhzx222/luci-app-wechatpush
-  rm -rf package/feeds/luci/luci-theme-argon
-  git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/xzhhzx222/luci-theme-argon
-fi
+# if [[ $BUILD_BRANCH == openwrt-* ]]; then
+# elif [ $BUILD_BRANCH = lede ]; then
+# fi
+
+case $BUILD_BRANCH in
+  openwrt-*)
+    LOGO_FILE=package/xzhhzx222/luci-app-wechatpush/root/usr/share/wechatpush/api/logo.jpg
+    SET_FILE=package/default-settings/files/zzz-default-settings
+    rm -rf package/network/config/firewall
+    rm -rf package/network/utils/iptables
+    rm -rf package/feeds/luci/luci-app-firewall
+    rm -rf package/feeds/lienol/luci-app-softethervpn
+    #svn export https://github.com/coolsnowwolf/lede/trunk/package/network/config/firewall package/network/config/firewall
+    svn export https://github.com/immortalwrt/immortalwrt/trunk/package/network/config/firewall package/network/config/firewall
+    # svn export https://github.com/coolsnowwolf/lede/trunk/package/network/utils/iptables package/network/utils/iptables
+    svn export https://github.com/immortalwrt/immortalwrt/trunk/package/network/utils/iptables package/network/utils/iptables
+    # svn export https://github.com/coolsnowwolf/lede/trunk/package/network/services/fullconenat package/network/services/fullconenat
+    svn export https://github.com/immortalwrt/immortalwrt/trunk/package/network/utils/fullconenat package/network/utils/fullconenat
+    # svn export https://github.com/coolsnowwolf/luci/trunk/applications/luci-app-firewall package/feeds/luci/luci-app-firewall
+    svn export https://github.com/immortalwrt/luci/trunk/applications/luci-app-firewall package/feeds/luci/luci-app-firewall
+    # svn export https://github.com/coolsnowwolf/luci/trunk/luci.mk package/feeds/luci.mk
+    svn export https://github.com/immortalwrt/luci/trunk/luci.mk package/feeds/luci.mk
+     svn export https://github.com/Lienol/openwrt/branches/22.03/package/default-settings package/default-settings
+    #svn export https://github.com/coolsnowwolf/lede/trunk/package/lean/default-settings package/default-settings
+    svn export https://github.com/coolsnowwolf/luci/trunk/applications/luci-app-softethervpn package/xzhhzx222/luci-app-softethervpn
+    git clone https://github.com/tty228/luci-app-wechatpush.git package/xzhhzx222/luci-app-wechatpush
+    git clone https://github.com/jerrykuku/luci-theme-argon.git package/xzhhzx222/luci-theme-argon
+    sed -i '/LUCI_DEPENDS:=/s/$/ +@LUCI_LANG_zh_Hans/' package/feeds/luci/luci-base/Makefile
+    for pkg in $(find package/feeds/*/luci-app*/po -maxdepth 0 -type d); do
+      if [ ! -d "$pkg/zh_Hans" ] && [ -d "$pkg/zh-cn" ]; then 
+        ln -sf zh-cn "$pkg/zh_Hans"
+      fi
+    done
+    ;;
+  lienol)
+    LOGO_FILE=package/xzhhzx222/luci-app-wechatpush/root/usr/share/wechatpush/api/logo.jpg
+    SET_FILE=package/default-settings/files/zzz-default-settings
+    rm -rf package/feeds/lienol/luci-app-softethervpn
+    svn export https://github.com/coolsnowwolf/luci/trunk/applications/luci-app-softethervpn package/xzhhzx222/luci-app-softethervpn
+    git clone https://github.com/tty228/luci-app-wechatpush.git package/xzhhzx222/luci-app-wechatpush
+    rm -rf package/feeds/luci/luci-theme-argon
+    git clone https://github.com/jerrykuku/luci-theme-argon.git package/xzhhzx222/luci-theme-argon
+    ;;
+  lede)
+    LOGO_FILE=package/xzhhzx222/luci-app-wechatpush/root/usr/share/serverchan/api/logo.jpg
+    SET_FILE=package/lean/default-settings/files/zzz-default-settings
+    # wget -O package/feeds/packages/ddns-scripts/files/update_cloudflare_com_v4.sh https://raw.githubusercontent.com/openwrt/packages/master/net/ddns-scripts/files/usr/lib/ddns/update_cloudflare_com_v4.sh
+    git clone -b openwrt-18.06 https://github.com/tty228/luci-app-wechatpush.git package/xzhhzx222/luci-app-wechatpush
+    rm -rf package/feeds/luci/luci-theme-argon
+    git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/xzhhzx222/luci-theme-argon
+    ;;
+esac
 
 echo "------------ Check Start ------------"
 echo "LOGO_FILE=$LOGO_FILE"
