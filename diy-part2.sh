95bce5bf74ba2eee07e1609c25695939c0246247
xzhhzx222 <xzhhzx222@gmail.com>
2023-09-28 12:59:40 +0800

    update

--- diy-part2.sh
+++ diy-part2.sh
@@ -31,7 +31,9 @@ case $BUILD_BRANCH in
     LOGO_FILE=package/feeds/luci/luci-app-wechatpush/root/usr/share/wechatpush/api/logo.jpg
     SET_FILE=package/emortal/default-settings/files/99-default-settings
     rm -rf package/feeds/luci/luci-app-openclash
+    rm -rf package/feeds/luci/luci-app-passwall
     rm -rf package/feeds/luci/luci-app-ssr-plus
+    rm -rf package/feeds/luci/luci-app-vssr
     rm -rf package/feeds/luci/luci-theme-argon
     git clone https://github.com/jerrykuku/luci-theme-argon.git package/xzhhzx222/luci-theme-argon
     ;;
