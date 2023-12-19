318eb05316954d43e426bc3efa5922d33d48ee15
xzhhzx222 <xzhhzx222@gmail.com>
2023-12-19 16:59:44 +0800

    update

--- diy-part1.sh
+++ diy-part1.sh
@@ -11,50 +11,36 @@
 #
 
 # Uncomment a feed source
-# sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-# echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-# echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+#echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 
-#
 # 添加fw876/helloworld
-#
 if [[ $BUILD_BRANCH == lede-* ]]; then
   echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 fi
 
-# #
-# # 添加xiaorouji/openwrt-passwall
-# #
+# 添加xiaorouji/openwrt-passwall
 # echo 'src-git passwall_pkg https://github.com/xiaorouji/openwrt-passwall;packages' >>feeds.conf.default
 # echo 'src-git passwall_luci https://github.com/xiaorouji/openwrt-passwall;luci' >>feeds.conf.default
 # echo 'src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2' >>feeds.conf.default
 
-# #
-# # 添加sundaqiang/openwrt-packages
-# #
+# 添加sundaqiang/openwrt-packages
 # echo 'src-git sundaqiang https://github.com/sundaqiang/openwrt-packages;master' >>feeds.conf.default
 
-#
 # 添加sirpdboy/luci-app-advanced
-#
 git clone https://github.com/sirpdboy/luci-app-advanced.git package/xzhhzx222/luci-app-advanced
 
-# #
-# # 添加jerrykuku/luci-app-vssr
-# #
+# 添加jerrykuku/luci-app-vssr
 # git clone https://github.com/jerrykuku/lua-maxminddb.git package/xzhhzx222/lua-maxminddb
 # git clone https://github.com/jerrykuku/luci-app-vssr.git package/xzhhzx222/luci-app-vssr
 
-# #
-# # 添加destan19/OpenAppFilter
-# #
+# 添加destan19/OpenAppFilter
 # git clone https://github.com/destan19/OpenAppFilter.git package/xzhhzx222/OpenAppFilter
 
-#
 # 添加vernesong/OpenClash
-#
 CLASH_DIR=package/xzhhzx222/OpenClash/luci-app-openclash/root/etc/openclash
 
 echo "------------ Check Start ------------"
