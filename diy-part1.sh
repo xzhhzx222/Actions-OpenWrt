b27df79a86b468e15e5bec33b9260f48ce67fbb3
xzhhzx222 <xzhhzx222@gmail.com>
2023-12-19 17:14:16 +0800

    update

--- diy-part1.sh
+++ diy-part1.sh
@@ -29,28 +29,3 @@ fi
 
 # 添加sundaqiang/openwrt-packages
 # echo 'src-git sundaqiang https://github.com/sundaqiang/openwrt-packages;master' >>feeds.conf.default
-
-# 添加sirpdboy/luci-app-advanced
-git clone https://github.com/sirpdboy/luci-app-advanced.git package/xzhhzx222/luci-app-advanced
-
-# 添加jerrykuku/luci-app-vssr
-# git clone https://github.com/jerrykuku/lua-maxminddb.git package/xzhhzx222/lua-maxminddb
-# git clone https://github.com/jerrykuku/luci-app-vssr.git package/xzhhzx222/luci-app-vssr
-
-# 添加destan19/OpenAppFilter
-# git clone https://github.com/destan19/OpenAppFilter.git package/xzhhzx222/OpenAppFilter
-
-# 添加vernesong/OpenClash
-CLASH_DIR=package/xzhhzx222/OpenClash/luci-app-openclash/root/etc/openclash
-echo "------------ Check Start ------------"
-echo "CLASH_DIR=$CLASH_DIR"
-echo "------------- Check End -------------"
-git clone --depth=1 https://github.com/vernesong/OpenClash.git package/xzhhzx222/OpenClash
-rm -vrf $CLASH_DIR/Geo*
-curl -Ls -o $CLASH_DIR/GeoIP.dat https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geoip.dat
-curl -Ls -o $CLASH_DIR/GeoSite.dat https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geosite.dat
-mkdir -p $CLASH_DIR/core
-curl -Ls -o $CLASH_DIR/core/core.tar.gz https://raw.githubusercontent.com/vernesong/OpenClash/core/master/meta/clash-linux-amd64.tar.gz
-tar zxf $CLASH_DIR/core/core.tar.gz -C $CLASH_DIR/core
-mv -v $CLASH_DIR/core/clash $CLASH_DIR/core/clash_meta
-rm -vf $CLASH_DIR/core/core.tar.gz
--- diy-part2.sh
+++ diy-part2.sh
@@ -11,29 +11,61 @@
 #
 
 # Modify default IP
-# sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 
+# 删除已存在的软件包
 rm -vrf package/feeds/luci/luci-app-openclash
-# rm -vrf package/feeds/luci/luci-app-passwall
+rm -vrf package/feeds/luci/luci-app-passwall
 rm -vrf package/feeds/luci/luci-app-serverchan
-# rm -vrf package/feeds/luci/luci-app-vssr
+rm -vrf package/feeds/luci/luci-app-vssr
 rm -vrf package/feeds/luci/luci-app-wechatpush
 rm -vrf package/feeds/luci/luci-theme-argon*
-sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' package/feeds/luci/luci/Makefile
 
+# 添加sirpdboy/luci-app-advanced
+git clone https://github.com/sirpdboy/luci-app-advanced.git package/xzhhzx222/luci-app-advanced
+
+# 添加Lienol/luci-app-control系列
 svn export https://github.com/Lienol/openwrt-package/trunk/luci-app-control-timewol package/xzhhzx222/luci-app-control-timewol
 svn export https://github.com/Lienol/openwrt-package/trunk/luci-app-control-weburl package/xzhhzx222/luci-app-control-weburl
+
+# 添加vernesong/OpenClash
+CLASH_DIR=package/xzhhzx222/OpenClash/luci-app-openclash/root/etc/openclash
+
+echo "------------ Check Start ------------"
+echo "CLASH_DIR=$CLASH_DIR"
+echo "------------- Check End -------------"
+
+git clone --depth=1 https://github.com/vernesong/OpenClash.git package/xzhhzx222/OpenClash
+rm -vrf $CLASH_DIR/Geo*
+curl -Ls -o $CLASH_DIR/GeoIP.dat https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geoip.dat
+curl -Ls -o $CLASH_DIR/GeoSite.dat https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geosite.dat
+mkdir -p $CLASH_DIR/core
+curl -Ls -o $CLASH_DIR/core/core.tar.gz https://raw.githubusercontent.com/vernesong/OpenClash/core/master/meta/clash-linux-amd64.tar.gz
+tar zxf $CLASH_DIR/core/core.tar.gz -C $CLASH_DIR/core
+mv -v $CLASH_DIR/core/clash $CLASH_DIR/core/clash_meta
+rm -vf $CLASH_DIR/core/core.tar.gz
+
+# 添加Lienol/luci-app-timecontrol
 svn export https://github.com/Lienol/openwrt-package/trunk/luci-app-timecontrol package/xzhhzx222/luci-app-timecontrol
+
+# 添加jerrykuku/luci-app-vssr
+# git clone https://github.com/jerrykuku/lua-maxminddb.git package/xzhhzx222/lua-maxminddb
+# git clone https://github.com/jerrykuku/luci-app-vssr.git package/xzhhzx222/luci-app-vssr
+
+# 修改默认主题
+sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' package/feeds/luci/luci/Makefile
 
 if [[ $BUILD_BRANCH == immortalwrt-* ]]; then
   LOGO_FILE=package/xzhhzx222/luci-app-wechatpush/root/usr/share/wechatpush/api/logo.jpg
   SET_FILE=package/emortal/default-settings/files/99-default-settings
   sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' package/feeds/luci/luci-light/Makefile
-  git clone https://github.com/tty228/luci-app-wechatpush.git package/xzhhzx222/luci-app-wechatpush
   # sed -i 's/+IPV6:.* //' package/feeds/packages/miniupnpd/Makefile
+  #
   # echo "------------ Check Start ------------"
   # grep "DEPENDS:=" package/feeds/packages/miniupnpd/Makefile
   # echo "------------- Check End -------------"
+  #
+  git clone https://github.com/tty228/luci-app-wechatpush.git package/xzhhzx222/luci-app-wechatpush
   # sed -i 's/\${str_linefeed}/\\\\n/g' package/xzhhzx222/luci-app-wechatpush/root/usr/share/wechatpush/api/qywx_mpnews.json
   sed -i 's/\${1} ${nowtime}/${nowtime}\\\\n${1}/g' package/xzhhzx222/luci-app-wechatpush/root/usr/share/wechatpush/api/qywx_mpnews.json
   git clone https://github.com/jerrykuku/luci-theme-argon.git package/xzhhzx222/luci-theme-argon
