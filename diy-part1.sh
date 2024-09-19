2020380d5a447ec4e12455303c102b44689f3d09
xzhhzx222 <xzhhzx222@gmail.com>
2024-09-19 09:48:35 +0800

    update

--- diy-part1.sh
+++ diy-part1.sh
@@ -14,21 +14,24 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+#echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 
 
 # 更改luci/openwrt-23.05
 if [[ $BUILD_BRANCH == lede-23.05 ]]; then
-  sed -i 's#\/luci#&.git;openwrt-23.05#g' feeds.conf.default
-  # sed -i '/luci/d' feeds.conf.default
-  # echo 'src-git luci https://github.com/openwrt/luci.git;openwrt-23.05' >>feeds.conf.default
+  # sed -i 's#\/luci#&.git;openwrt-23.05#g' feeds.conf.default
+  sed -i '/luci/d' feeds.conf.default
+  echo 'src-git luci https://github.com/openwrt/luci.git;openwrt-23.05' >>feeds.conf.default
 fi
 
+# 添加fw876/helloworld
+echo 'src-git helloworld https://github.com/fw876/helloworld.git' >>feeds.conf.default
+
 # 添加xiaorouji/openwrt-passwall
-# echo 'src-git passwall_pkg https://github.com/xiaorouji/openwrt-passwall;packages' >>feeds.conf.default
-# echo 'src-git passwall_luci https://github.com/xiaorouji/openwrt-passwall;luci' >>feeds.conf.default
-# echo 'src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2' >>feeds.conf.default
+# echo 'src-git passwall_pkg https://github.com/xiaorouji/openwrt-passwall.git;packages' >>feeds.conf.default
+# echo 'src-git passwall_luci https://github.com/xiaorouji/openwrt-passwall.git;luci' >>feeds.conf.default
+# echo 'src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2.git' >>feeds.conf.default
 
 # 添加sundaqiang/openwrt-packages
-echo 'src-git sundaqiang https://github.com/sundaqiang/openwrt-packages;master' >>feeds.conf.default
+echo 'src-git sundaqiang https://github.com/sundaqiang/openwrt-packages.git;master' >>feeds.conf.default
