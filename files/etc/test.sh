0e9b88cc561defc7a3ac43aec6d71dcf02710038
xzhhzx222 <xzhhzx222@gmail.com>
2025-09-25 17:29:32 +0800

    update

---
+++ files/etc/test.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+# homeconf.sh
+
+samwrt() {
+    chmod +x /etc/samwrt.sh
+    sh /etc/samwrt.sh
+}
+
+jimmywrt() {
+    chmod +x /etc/jimmywrt.sh
+    sh /etc/jimmywrt.sh
+}
+
+echo "Load Settings:"
+echo "1) SamWrt"
+echo "2) JimmyWrt"
+echo "0) Exit"
+
+read -p "Please select: " choice
+
+case "$choice" in
+    1) samwrt ;;
+    2) jimmywrt ;;
+    *) exit 0 ;;
+esac
+
